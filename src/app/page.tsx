f4fbb104ba6f264ac3970c0ff7f2c2d9bbe56301
Joe Balzac <joebalzac@Mac.lan>
2025-01-29 23:52:48 -0500

    adds lots of functions fo calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode, useEffect, useState } from "react";
 import Modal from "./Components/Modal";
+import { styleText } from "util";
 
 interface Reminder {
   id: number;
@@ -16,7 +17,13 @@ export default function Home() {
   const [newReminder, setNewReminder] = useState("");
   const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isReminderModalOpen, setIsReminderModalOpen] = useState(false);
   const [selectedDay, setSelectedDay] = useState<Date | null>(null);
+  const [isReminderSelected, setIsReminderSelected] = useState<number | null>(
+    null
+  );
+  const [editingId, setEditingId] = useState<Number | null>(null);
+  const [editingText, setEditingText] = useState("");
 
   const getDaysInMonth = (year: number, month: number): Date[] => {
     const days: Date[] = [];
@@ -56,6 +63,7 @@ export default function Home() {
   };
 
   const handleDayClick = (e: React.MouseEvent, day: Date) => {
+    e.preventDefault();
     const rect = (e.target as HTMLElement).getBoundingClientRect();
     setModalPosition({
       top: rect.top + window.scrollY,
@@ -66,22 +74,63 @@ export default function Home() {
     console.log("this day click is being triggered by this day", day);
   };
 
+  const handleReminderClick = (
+    e:
+      | React.MouseEvent<HTMLButtonElement>
+      | React.KeyboardEvent<HTMLButtonElement>,
+    reminder: Reminder
+  ) => {
+    e.preventDefault();
+    setIsReminderSelected(reminder.id);
+    setIsReminderModalOpen(true);
+  };
+
   {
     console.log("selected day", selectedDay?.toDateString());
   }
 
+  const handleDeleteReminder = (id: number) => {
+    setReminders(reminders.filter((reminder) => reminder.id !== id));
+    console.log("this is deleting");
+  };
+
+  const handleSelectReminder = (id: number) => {
+    setIsReminderSelected(id);
+  };
+
+  const handleEditReminder = (id: number, title: string) => {
+    setEditingId(id);
+    setEditingText(title);
+  };
+
+  const handleSaveReminder = () => {
+    if (editingText !== null) {
+      setReminders(
+        reminders.map((reminder) =>
+          reminder.id === editingId
+            ? { ...reminder, title: editingText }
+            : reminder
+        )
+      );
+      setEditingText("");
+    }
+  };
+
   return (
     <>
-      <div className="flex justify-center items-center h-screen w-9/12 mx-auto">
+      <div className="flex flex-col justify-center items-center h-screen w-9/12 mx-auto">
+        <h1 className="text-3xl font-semibold text-gray-900 mb-4">
+          {new Date().toLocaleString("default", { month: "long" })}
+        </h1>
         <ul className="w-full grid grid-cols-2 md:grid-cols-5 lg:grid-cols-7 gap-1">
           {calendarDays.map((day) => (
             <div key={day.toISOString()}>
               <div className="relative">
                 <li
-                  className={`text-2xl w-full h-40 border-purple-900 shadow-lg cursor-pointer hover:bg-purple-500 hover:shadow-purple-500 hover:text-white rounded-md flex flex-col items-center justify-center ${
+                  className={`w-full h-32 border border-gray-300 rounded-lg p-2 flex flex-col items-center justify-between text-lg transition duration-200 shadow-sm cursor-pointer hover:bg-gray-100 ${
                     day.toDateString() === todayString
-                      ? "text-red-600 font-semibold"
-                      : "text-gray-900"
+                      ? "bg-purple-500 text-white font-bold"
+                      : "bg-white"
                   }`}
                   onDoubleClick={(e) => handleDayClick(e, day)}
                 >
@@ -93,9 +142,29 @@ export default function Home() {
                         day.toDateString()
                     )
                     .map((reminder) => (
-                      <li className="bg-purple-400 text-white text-sm w-full p-1 list-item my-1">
+                      <button
+                        key={reminder.id}
+                        onClick={() => handleSelectReminder(reminder.id)}
+                        onDoubleClick={(e) => handleReminderClick(e, reminder)}
+                        onKeyDown={(e) => {
+                          if (e.key === "Backspace") {
+                            handleDeleteReminder(reminder.id);
+                          }
+                          if (e.key === "Enter") {
+                            handleReminderClick(e, reminder);
+                          }
+                        }}
+                        tabIndex={0}
+                        className={`w-full text-white text-xs px-2 py-1 rounded-lg truncate ${
+                          isReminderSelected === reminder.id
+                            ? "bg-purple-700"
+                            : "bg-purple-400"
+                        }
+                      
+                    text-white text-sm text-left w-full px-1  my-1 cursor-default flex justify-between `}
+                      >
                         {reminder.title}
-                      </li>
+                      </button>
                     ))}
                 </li>
               </div>
@@ -103,29 +172,72 @@ export default function Home() {
           ))}
         </ul>
 
-        <Modal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          position={modalPosition}
-        >
-          <h2 className="text-md font-semibold text-gray-900">Add Reminder:</h2>
-          <h3 className="text-1xl font-medium text-gray-800 mb-1">
-            Reminders for {selectedDay?.toDateString()}{" "}
-          </h3>
-          <input
-            value={newReminder}
-            type="text"
-            onChange={(e) => setNewReminder(e.target.value)}
-            placeholder="Add Reminder"
-            className="bg-transparent border-b-2 w-full px-2"
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && newReminder.trim() !== "") {
-                handleAddReminders();
-                setNewReminder("");
-              }
-            }}
-          />
-        </Modal>
+        {isReminderModalOpen === true ? (
+          <div>
+            <Modal
+              isOpen={isReminderModalOpen}
+              onClose={() => setIsReminderModalOpen(false)}
+              position={modalPosition}
+            >
+              {reminders.map((reminder) =>
+                selectedDay && reminder.id === isReminderSelected ? (
+                  <div key={reminder.id}>
+                    {reminder.title}
+                    <div className="Flex items-center justify-between gap-6">
+                      <button
+                        className="border-gray-900 border-2 text-gray-900 rounded-md py-1 px-4"
+                        onClick={() =>
+                          handleEditReminder(reminder.id, reminder.title)
+                        }
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="border-red-600 border-2 text-red-600 rounded-md py-1 px-4"
+                        onClick={handleSaveReminder}
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                ) : null
+              )}
+              <input
+                type="text"
+                value={editingText}
+                onChange={(e) => setEditingText(e.target.value)}
+              />
+            </Modal>
+          </div>
+        ) : (
+          <div>
+            <Modal
+              isOpen={isModalOpen}
+              onClose={() => setIsModalOpen(false)}
+              position={modalPosition}
+            >
+              <h2 className="text-md font-semibold text-gray-900">
+                Add Reminder:
+              </h2>
+              <h3 className="text-1xl font-medium text-gray-800 mb-1">
+                Reminders for {selectedDay?.toDateString()}{" "}
+              </h3>
+              <input
+                value={newReminder}
+                type="text"
+                onChange={(e) => setNewReminder(e.target.value)}
+                placeholder="Add Reminder"
+                className="bg-transparent border-b-2 w-full px-2"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && newReminder.trim() !== "") {
+                    handleAddReminders();
+                    setNewReminder("");
+                  }
+                }}
+              />
+            </Modal>
+          </div>
+        )}
       </div>
     </>
   );
