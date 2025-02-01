06b009172bc12073ea39f550bf8843948f712f10
Joe Balzac <jgbalzac@gmail.com>
2025-01-31 22:25:42 -0500

    fixes back to today

--- src/Components/ReminderModal.tsx
+++ src/Components/ReminderModal.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Modal from "./Modal";
 
 interface ReminderModalProps {
   isOpen: boolean;
   onClose: () => void;
   onAddReminder: () => void;
+  onDeleteReminder: (id: number) => void;
   newReminder: string;
   setNewReminder: (text: string) => void;
+  position: { top: number; left: number };
 }
 
 const ReminderModal: React.FC<ReminderModalProps> = ({
@@ -14,17 +17,27 @@ const ReminderModal: React.FC<ReminderModalProps> = ({
   onAddReminder,
   newReminder,
   setNewReminder,
+  onDeleteReminder,
+  position,
 }) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} position={{ top: 0, left: 0 }}>
+    <Modal isOpen={isOpen} onClose={onClose} position={position}>
       <h2 className="text-lg font-semibold">Add Reminder</h2>
       <input
         value={newReminder}
         type="text"
         onChange={(e) => setNewReminder(e.target.value)}
         placeholder="Add Reminder"
-        className="border-b-2 w-full px-2"
-        onKeyDown={(e) => e.key === "Enter" && onAddReminder()}
+        className="border-b-2 w-full px-1 cursor-default"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onAddReminder();
+          }
+
+          if (e.key === "Backspace") {
+            onDeleteReminder(0); 
+          }
+        }}
       />
     </Modal>
   );
